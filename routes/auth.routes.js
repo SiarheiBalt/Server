15b35b5e3509776ea034b5a81e2b812d1ab04f99
SiarheiBalt <jh-11@yandex.by>
2021-10-29 17:30:57 +0300

    feat: add file utils

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,23 +1,20 @@
 const { Router } = require('express');
 const bcrypt = require('bcryptjs');
-const config = require('config');
-const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 const router = Router();
 const { check, validationResult } = require('express-validator');
+const { createUser, createToken } = require('./utils/authUtils');
 
 //  /api/auth/register
 router.post(
   '/register',
   [
-    //валидация
     check('email', 'Incorrect email').isEmail(),
     check('password', 'Minimum length 6 characters').isLength({ min: 6 }),
     check('name', 'Minimum length 3 characters').isLength({ min: 3 }),
   ],
 
   async (req, res) => {
-    // console.log('body:', req.body);
     try {
       const errors = validationResult(req);
       if (!errors.isEmpty()) {
@@ -27,17 +24,14 @@ router.post(
         });
       }
       const { email, password, name } = req.body;
-      const candidate = await User.findOne({ email }); // Проверка есть ли такой email
+      const candidate = await User.findOne({ email });
       if (candidate) {
         return res.status
           .apply(400)
           .json({ message: 'This user already exists' });
       }
 
-      const hashePassword = await bcrypt.hash(password, 12); //шифрование пароля
-      const user = new User({ email, password: hashePassword, name }); //создание пользователя
-
-      await user.save();
+      createUser(email, password, name);
 
       res.status(201).json({ message: 'User created' });
     } catch (err) {
@@ -50,7 +44,6 @@ router.post(
 router.post(
   '/login',
   [
-    //валидатор - можно удалить и написать на фронте
     check('email', 'Please enter a valid email').normalizeEmail().isEmail(),
     check('password', 'Enter password').exists(),
   ],
@@ -63,29 +56,22 @@ router.post(
           message: 'Incorrect login data',
         });
       }
-
       const { email, password } = req.body;
-
-      const user = await User.findOne({ email }); //Ищу пользователя
+      const user = await User.findOne({ email });
 
       if (!user) {
         return res.status(400).json({ message: 'User is not found' });
       }
-      const isMatch = await bcrypt.compare(password, user.password); //Сравниваю пароли
+      const isMatch = await bcrypt.compare(password, user.password);
 
       if (!isMatch) {
         return res
           .status(400)
           .json({ message: 'Invalid password please try again' });
       }
-      // формирование токена
-      const token = jwt.sign(
-        { userId: user.id }, // данные которые будут зашифрованы
-        config.get('jwtSecret'), //секретный ключ
-        { expiresIn: '1h' } //через сколько jwt токен перестанет существовать
-      );
+      const token = createToken(user);
 
-      res.json({ token, userId: user.id, name: user.name }); //передаю на фронт
+      res.json({ token, userId: user.id, name: user.name });
     } catch (err) {
       res.status(500).json({ message: 'Something went wrong' });
     }
@@ -93,5 +79,3 @@ router.post(
 );
 
 module.exports = router;
-
-// Router is middleWare
