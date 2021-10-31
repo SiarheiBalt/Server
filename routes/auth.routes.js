ddbc17e022ca7a8c6e1dfdf197b7ffe001d3ff8b
SiarheiBalt <jh-11@yandex.by>
2021-10-31 20:22:47 +0300

    feat: add middleware for incorrect input

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,9 +1,12 @@
 const { Router } = require('express');
-const bcrypt = require('bcryptjs');
 const User = require('../models/User');
 const router = Router();
-const { check, validationResult } = require('express-validator');
-const { createUser, createToken } = require('./utils/authUtils');
+const { check } = require('express-validator');
+const { createUser, createToken } = require('../utils/auth.utils');
+const {
+  checkRegistrationError,
+  checkLoginError,
+} = require('../middleWare/auth.middleware');
 
 //  /api/auth/register
 router.post(
@@ -13,30 +16,11 @@ router.post(
     check('password', 'Minimum length 6 characters').isLength({ min: 6 }),
     check('name', 'Minimum length 3 characters').isLength({ min: 3 }),
   ],
-
-  async (req, res) => {
-    try {
-      const errors = validationResult(req);
-      if (!errors.isEmpty()) {
-        return res.status(400).json({
-          errors: errors.array(),
-          message: 'Incorrect registration data',
-        });
-      }
-      const { email, password, name } = req.body;
-      const candidate = await User.findOne({ email });
-      if (candidate) {
-        return res.status
-          .apply(400)
-          .json({ message: 'This user already exists' });
-      }
-
-      createUser(email, password, name);
-
-      res.status(201).json({ message: 'User created' });
-    } catch (err) {
-      res.status(500).json({ message: 'This user already exists' });
-    }
+  checkRegistrationError,
+  (req, res) => {
+    const { email, password, name } = req.body;
+    createUser(email, password, name);
+    res.status(201).json({ message: 'User created' });
   }
 );
 
@@ -47,34 +31,12 @@ router.post(
     check('email', 'Please enter a valid email').normalizeEmail().isEmail(),
     check('password', 'Enter password').exists(),
   ],
+  checkLoginError,
   async (req, res) => {
-    try {
-      const errors = validationResult(req);
-      if (!errors.isEmpty()) {
-        return res.status(400).json({
-          errors: errors.array(),
-          message: 'Incorrect login data',
-        });
-      }
-      const { email, password } = req.body;
-      const user = await User.findOne({ email });
-
-      if (!user) {
-        return res.status(400).json({ message: 'User is not found' });
-      }
-      const isMatch = await bcrypt.compare(password, user.password);
-
-      if (!isMatch) {
-        return res
-          .status(400)
-          .json({ message: 'Invalid password please try again' });
-      }
-      const token = createToken(user);
-
-      res.json({ token, userId: user.id, name: user.name });
-    } catch (err) {
-      res.status(500).json({ message: 'Something went wrong' });
-    }
+    const { email } = req.body;
+    const user = await User.findOne({ email });
+    const token = createToken(user);
+    res.json({ token, userId: user.id, name: user.name });
   }
 );
 
--- utils/auth.utils.js
+++ utils/auth.utils.js
@@ -1,5 +1,5 @@
 const bcrypt = require('bcryptjs');
-const User = require('../../models/User');
+const User = require('../models/User');
 const config = require('config');
 const jwt = require('jsonwebtoken');
 
